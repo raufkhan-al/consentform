9a08eab8b4b250620c53312560b9df4fb22fab50
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2021-07-13 01:34:00 +0100

    phpmailer added

--- index.php
+++ index.php
@@ -1,4 +1,150 @@
-<!doctype html>
+<?php 
+
+    //Import PHPMailer classes into the global namespace
+    //These must be at the top of your script, not inside a function
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $name1 = $_POST['name1'];
+        $signature1 = $_POST['signature1'];
+        $date1 = $_POST['date1'];
+        $name2 = $_POST['name1'];
+        $signature2 = $_POST['signature1'];
+        $date2 = $_POST['date1'];
+        $name3 = $_POST['name1'];
+        $signature3 = $_POST['signature1'];
+        $date3 = $_POST['date1'];
+        $name4 = $_POST['name1'];
+        $signature4 = $_POST['signature1'];
+        $date4 = $_POST['date1'];
+
+        $message = '
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Consent Form</title>
+        </head>
+        <body>
+            <style>
+                table{
+                    border-collapse: collapse;
+                    padding: 20px 20px;
+                }
+                td,th {
+                    text-align: center;
+                    border: 1px solid #dddddd;
+                    text-align: left;
+                    padding: 8px;
+                }
+            </style>
+            <table style="width:100%">
+                <thead>
+                    <tr>
+                        <th>Name1</th>
+                        <th>Signature1</th>
+                        <th>Date1</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>'.$name1.'</td>
+                        <td>'.$signature1.'</td>
+                        <td>'.$date1.'</td>
+                    </tr>
+                </tbody>
+                <thead>
+                    <tr>
+                        <th>Name2</th>
+                        <th>Signature2</th>
+                        <th>Date2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>'.$name2.'</td>
+                        <td>'.$signature2.'</td>
+                        <td>'.$date2.'</td>
+                    </tr>
+                </tbody>
+                <thead>
+                    <tr>
+                        <th>Name3</th>
+                        <th>Signature3</th>
+                        <th>Date3</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>'.$name3.'</td>
+                        <td>'.$signature3.'</td>
+                        <td>'.$date3.'</td>
+                    </tr>
+                </tbody>
+                <thead>
+                    <tr>
+                        <th>Name4</th>
+                        <th>Signature4</th>
+                        <th>Date4</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>'.$name4.'</td>
+                        <td>'.$signature4.'</td>
+                        <td>'.$date4.'</td>
+                    </tr>
+                </tbody>
+            </table>
+        </body>
+        </html>';
+
+        try {
+        //Server settings
+        $mail->SMTPDebug = 0;    // 0=no output, 1=Commands, 2=Data & Commands, 3=2+connection status 4=Low-Level data output
+        $mail->isSMTP();                           // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';            // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                    // Enable SMTP authentication
+        $mail->Username = 'hauwal4969@gmail.com'; // SMTP username
+        $mail->Password = 'oarwkxrbvipazotz';      // SMTP password (used google Generated app password) 
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+            )
+            );
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;                 // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 465;                         // TCP port to connect to
+
+        $mail->isHTML(true);   
+
+        $mail->setFrom('hauwal4969@gmail.com');
+        $mail->addAddress('hauwal4969@gmail.com'); 
+        //Send HTML or Plain Text email
+        $mail->isHTML(true);
+        $mail->Subject = "New Consent Form Submission"; 
+        $mail->Body = $message;
+
+        $mail->send();
+        } 
+        catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+    
+    
+ ?>
+ <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -86,7 +232,7 @@
     </div>
     <div class="row pujatop">
     </div>
-    <form class="form-horizontal" action="sendEmail.php" id="contactForm" method="post">
+    <form class="form-horizontal" action="" id="contactForm" method="post">
         <div class="row para">
             <p style="font-size: 22px; font-weight: 900px; padding-top: 10px;">
                 |, <input type="text" style="width: 300px;" name="name1" id="exampleInputFile">&nbsp;&nbsp;&nbsp;(print name)give permission to Polished Dental Hygiene Clinic to preform the dental hygiene procedures deemed necessary during my appointment.
